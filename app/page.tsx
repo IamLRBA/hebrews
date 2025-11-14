584a1f92567dc37a6cdfcffd24432552ff71140c
IamLRBA <jerrylarubafestus@gmail.com>
2025-11-14 18:15:45 +0300

    Scroll Indicator Update

--- app/page.tsx
+++ app/page.tsx
@@ -230,7 +230,7 @@ export default function Home() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ delay: 1.5 }}
-                  className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+                  className="absolute bottom-8 md:bottom-4 left-1/2 transform -translate-x-1/2"
                 >
                   <motion.div
                     animate={{ y: [0, 10, 0] }}
